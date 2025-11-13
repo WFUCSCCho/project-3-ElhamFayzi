56f338e34d6ec59cd350ba8745f96624a5c7d2f2
ElhamFayzi <fayze24@wfu.edu>
2025-11-12 23:20:43 -0500

    Add the running time tests for each algorithm

--- src/Proj3.java
+++ src/Proj3.java
@@ -1,10 +1,13 @@
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class Proj3 {
     // Sorting Method declarations
     // Merge Sort
-    public static <T extends Comparable> void mergeSort(ArrayList<T> a, int left, int right) {
+    public static <T extends Comparable<T>> void mergeSort(ArrayList<T> a, int left, int right) {
         if (left >= right) return;                      //FIXME!!! Check for all of the edge cases, e.g. a is empty or right > a.size()
 
         int mid = left + (right - left) / 2;
@@ -14,7 +17,7 @@ public class Proj3 {
         merge(a, left, mid, right);
     }
 
-    public static <T extends Comparable> void merge(ArrayList<T> a, int left, int mid, int right) {
+    public static <T extends Comparable<T>> void merge(ArrayList<T> a, int left, int mid, int right) {
         ArrayList<T> temp = new ArrayList<T>();
 
         int i = left;
@@ -46,7 +49,7 @@ public class Proj3 {
     }
 
     // Quick Sort
-    public static <T extends Comparable> void quickSort(ArrayList<T> a, int left, int right) {
+    public static <T extends Comparable<T>> void quickSort(ArrayList<T> a, int left, int right) {
         if (left >= right) return;
 
         //Find pivot
@@ -63,7 +66,7 @@ public class Proj3 {
     }
 
 
-    public static <T extends Comparable> int partition (ArrayList<T> a, int left, int right) {
+    public static <T extends Comparable<T>> int partition (ArrayList<T> a, int left, int right) {
         int pivot = right;
         int i = left;
         int j = right - 1;
@@ -93,9 +96,9 @@ public class Proj3 {
     }
 
     // Heap Sort
-    public static <T extends Comparable> void heapSort(ArrayList<T> a, int left, int right) {
+    public static <T extends Comparable<T>> void heapSort(ArrayList<T> a, int left, int right) {
         // Build MinHeap
-        for (int i = (right / 2) - 1; i >= left; i++) {
+        for (int i = (right / 2) - 1; i >= left; i--) {
             heapify(a, i, right);
         }
 
@@ -107,7 +110,7 @@ public class Proj3 {
 
     }
 
-    public static <T extends Comparable> void heapify (ArrayList<T> a, int left, int right) {
+    public static <T extends Comparable<T>> void heapify (ArrayList<T> a, int left, int right) {
         int min = left;
         int leftChild = 2 * min + 1;                               // leftChild is in index 2i on arrays that start from index 1, but since our array begins from 0th index, leftChild is at 2i + 1
         int rightChild = 2 * min + 2;
@@ -125,7 +128,7 @@ public class Proj3 {
     }
 
     // Bubble Sort
-    public static <T extends Comparable> int bubbleSort(ArrayList<T> a, int size) {
+    public static <T extends Comparable<T>> int bubbleSort(ArrayList<T> a, int size) {
         int numOfSwaps = 0;
 
         boolean isSorted = false;
@@ -145,7 +148,7 @@ public class Proj3 {
     }
 
     // Odd-Even Transposition Sort
-    public static <T extends Comparable> int transpositionSort(ArrayList<T> a, int size) {
+    public static <T extends Comparable<T>> int transpositionSort(ArrayList<T> a, int size) {
         int numSwapsIfParallel = 0;
 
         boolean isSorted = false;
@@ -174,13 +177,117 @@ public class Proj3 {
 
     public static void main(String [] args)  throws IOException {
         if (args.length != 3) {
-            System.out.println("Usage: java Proj3 [dataset name] [sorting algorithm] [number of lines]");
+            System.err.println("Usage: java Proj3 [dataset name] [sorting algorithm] [number of lines]");
+            System.exit(1);
         }
 
         String dataSet = args[0];
         String sortingAlgo = args[1];
-        int numOfLines = Integer.parseInt(args[2]);
+        int numLines = Integer.parseInt(args[2]);
+
+        FileInputStream fis = new FileInputStream(dataSet);
+        Scanner reader = new Scanner(fis);
+
+        // ignore first line
+        reader.nextLine();
+
+        ArrayList<Player> playersList = new ArrayList<Player>();
+
+        // Read each line and create Player objects
+        for (int i = 0; i < numLines; i++) {
+            if (!reader.hasNextLine()) {
+                System.err.println("The entered numLines is greater than the number of lines in file (" + i + " lines exist in the input file");
+                System.exit(1);
+            }
+            String[] line = reader.nextLine().toLowerCase().split(",");
+            try {
+                playersList.add(new Player(line));
+            } catch (Exception e) {
+                System.out.println("Line Number " + (i + 1) + " has an invalid format.");
+            }
+        }
+        reader.close();
 
 
+        long startTime;
+        long endTime;
+
+        int transpositionComparisons;
+        int bubbleComparisons;
+
+        //============================
+        // Measure running times for sorting an already-sorted list
+        Collections.sort(playersList);
+
+        startTime = System.nanoTime();
+        bubbleSort(playersList, playersList.size());
+        endTime = System.nanoTime();
+        double bubbleSort_SortedListTime = (endTime - startTime) / 1_000_000.0;
+
+        startTime = System.nanoTime();
+        mergeSort(playersList, 0, playersList.size() - 1);
+        endTime = System.nanoTime();
+        double mergeSort_SortedListTime = (endTime - startTime) / 1_000_000.0;
+
+        startTime = System.nanoTime();
+        heapSort(playersList, 0, playersList.size() - 1);
+        endTime = System.nanoTime();
+        double heapSort_SortedListTime = (endTime - startTime) / 1_000_000.0;
+
+        //============================
+        // Measure running times for sorting a randomly-shuffled list
+        Collections.shuffle(playersList);
+
+        startTime = System.nanoTime();
+        bubbleSort(playersList, playersList.size());
+        endTime = System.nanoTime();
+        double bubbleSort_shuffledListTime = (endTime - startTime) / 1_000_000.0;
+
+        startTime = System.nanoTime();
+        mergeSort(playersList, 0, playersList.size() - 1);
+        endTime = System.nanoTime();
+        double mergeSort_shuffledListTime = (endTime - startTime) / 1_000_000.0;
+
+        startTime = System.nanoTime();
+        heapSort(playersList, 0, playersList.size() - 1);
+        endTime = System.nanoTime();
+        double heapSort_shuffledListTime = (endTime - startTime) / 1_000_000.0;
+
+        //============================
+        // Measure running times for sorting a reversely-sorted list
+        Collections.sort(playersList, Collections.reverseOrder());
+
+        startTime = System.nanoTime();
+        bubbleSort(playersList, playersList.size());
+        endTime = System.nanoTime();
+        double bubbleSort_reversedListTime = (endTime - startTime) / 1_000_000.0;
+
+        startTime = System.nanoTime();
+        mergeSort(playersList, 0, playersList.size() - 1);
+        endTime = System.nanoTime();
+        double mergeSort_reversedListTime = (endTime - startTime) / 1_000_000.0;
+
+        startTime = System.nanoTime();
+        heapSort(playersList, 0, playersList.size() - 1);
+        endTime = System.nanoTime();
+        double heapSort_reversedListTime = (endTime - startTime) / 1_000_000.0;
+
+        //============================
+        // Measure number of comparisons for sorting an already-sorted list using bubble sort and transposition sort
+        Collections.sort(playersList);
+        transpositionComparisons = transpositionSort(playersList, playersList.size());
+        bubbleComparisons = bubbleSort(playersList, playersList.size());
+
+        //============================
+        // Measure number of comparisons for sorting a randomly-shuffled list using bubble sort and transposition sort
+        Collections.shuffle(playersList);
+        transpositionComparisons = transpositionSort(playersList, playersList.size());
+        bubbleComparisons = bubbleSort(playersList, playersList.size());
+
+        //============================
+        // Measure number of comparisons for sorting a reversely-sorted list using bubble sort and transposition sort
+        Collections.sort(playersList, Collections.reverseOrder());
+        transpositionComparisons = transpositionSort(playersList, playersList.size());
+        bubbleComparisons = bubbleSort(playersList, playersList.size());
     }
 }
